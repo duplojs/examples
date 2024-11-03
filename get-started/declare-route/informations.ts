2e3dfe23724321c733c7e8600de3dc214f6da030
Maubry94 <matthieu.aubry94370@gmail.com>
2024-11-03 18:13:28 +0100

    add info exemple

---
+++ get-started/declare-route/informations.ts
@@ -0,0 +1,11 @@
+import { useBuilder, Response } from "@duplojs/core";
+
+export const myRoute = useBuilder()
+    .createRoute("GET", "/hello-world")
+    .handler(() => {
+        return new Response(
+            200, 
+            "My super info!", 
+            undefined
+        );
+    });
